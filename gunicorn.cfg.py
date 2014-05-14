5572f693433ae75e25dda34794460372b106c4ef
Kostia <k@varik.ru>
2014-05-14 23:01:53 +0700

    f

--- gunicorn.cfg.py
+++ gunicorn.cfg.py
@@ -3,16 +3,22 @@
 
 # Workaround for PYTHONPATH problem https://github.com/migurski/TileStache/issues/86
 import sys
+import os
 import PIL.Image
 sys.modules['Image'] = PIL.Image
 
 bind='127.0.0.1:8000'
 
 # Number of worker processes: 2-4x number of CPU cores is recommended
-workers=2
 
-timeout=10000
- 
+if not hasattr(os, "sysconf"):
+  workers=1
+else:
+  os.sysconf("SC_NPROCESSORS_ONLN")
+
+
+timeout=10
+
 # Worker class. The default "sync" is fine with nginx as a proxy
 # gevent is a nice alternative if you want to serve the public from Gunicorn
 # worker_class='gevent'
