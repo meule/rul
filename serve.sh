7a685206f397c3c575563959b788ebde5ecb51a3
Kostia <k@varik.ru>
2014-05-15 02:33:15 +0700

    f

--- serve.sh
+++ serve.sh
@@ -24,8 +24,8 @@ sed -i.bak "s/${rep}/${ec2host}/g" index.html
 rep='_password_'
 sed -i.bak "s/${rep}/${psswrd}/g" tilestache.cfg
 
-sudo cp -R nginx-rul.conf /etc/nginx/sites-available/rul
+sudo cp -R nginx-rul.conf /etc/nginx/sites-available/default
 sudo cp -R index.html /var/www/rul/public_html
 sudo service nginx restart
-kill `cat /tmp/gunicorn.pid`
+kill `cat /tmp/gunicorn.pid` 
 gunicorn -c gunicorn.cfg.py -p /tmp/gunicorn.pid 'TileStache:WSGITileServer("tilestache.cfg")' &
