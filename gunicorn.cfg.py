b7c799e92a5b423db81d3b9d5a87e1379060edd9
Kostia <k@varik.ru>
2014-05-15 12:58:19 +0700

    f

--- gunicorn.cfg.py
+++ gunicorn.cfg.py
@@ -8,8 +8,7 @@ import PIL.Image
 sys.modules['Image'] = PIL.Image
 
 bind='127.0.0.1:8000'
- 
-# Number of worker processes: 2-4x number of CPU cores is recommended
+
 
 if not hasattr(os, "sysconf"):
   workers=1
@@ -18,7 +17,3 @@ else:
 
 
 timeout=30
-
-# Worker class. The default "sync" is fine with nginx as a proxy
-# gevent is a nice alternative if you want to serve the public from Gunicorn
-# worker_class='gevent'
