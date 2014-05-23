324e7a40d1bb8f907d587c02635ced6770672d66
Kostia <k@varik.ru>
2014-05-23 15:23:10 +0700

    styles

--- serve.sh
+++ serve.sh
@@ -1,15 +1,18 @@
+# bash rul.sh -e 54.72.184.190 -h osm.cxgbat4jt7jg.eu-west-1.rds.amazonaws.com -p rds_password -i
+
 rm tilestache.cfg
 rm index.html
 
 git pull
 # replace credentials in configs and html
-while getopts ":e:h:p:" option
+while getopts ":e:h:p:i:" option
 do
         case "${option}"
         in
             h) rdshost=${OPTARG};;
             e) ec2host=${OPTARG};;
             p) psswrd=${OPTARG};;
+            i) echo "index"
         esac
 done
 
@@ -23,7 +26,7 @@ rep='_ec2host_'
 sed -i.bak "s/${rep}/${ec2host}/g" index.html
 rep='_password_'
 sed -i.bak "s/${rep}/${psswrd}/g" tilestache.cfg
-  
+
 #sudo cp -R nginx-rul.conf /etc/nginx/sites-available/default
 sudo cp -R nginx-rul.conf /etc/nginx/sites-enabled
 #sudo cp -R nginx-rul.conf /etc/nginx/conf.d
