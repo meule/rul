ccb070c7a9475477e5fd135cbccca1d8666e5fce
Kostia <k@varik.ru>
2014-07-02 18:28:32 +0530

    r

--- serve.sh
+++ serve.sh
@@ -25,13 +25,14 @@ rep='_host_'
 sed -i.bak "s/${rep}/${rdshost}/g" tilestache.cfg
 rep='_ec2host_'
 sed -i.bak "s/${rep}/${ec2host}/g" index.html
+sed -i.bak "s/${rep}/${ec2host}/g" rivers.html
 rep='_password_'
 sed -i.bak "s/${rep}/${psswrd}/g" tilestache.cfg
 
 #sudo cp -R nginx-rul.conf /etc/nginx/sites-available/default
 sudo cp -R nginx-rul.conf /etc/nginx/sites-enabled
 #sudo cp -R nginx-rul.conf /etc/nginx/conf.d
-sudo cp -R index.html /var/www/rul/public_html
+sudo cp -R *.html /var/www/rul/public_html
 sudo service nginx restart
 kill `cat /tmp/gunicorn.pid`
 gunicorn -c gunicorn.cfg.py -p /tmp/gunicorn.pid 'TileStache:WSGITileServer("tilestache.cfg")' &
